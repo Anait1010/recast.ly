3a0ced40bd8f8eb329cfe129bb07443f78bee8cf
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-01-12 10:54:41 -0800

    Complete connecting Video List and Player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,9 +11,17 @@ class App extends React.Component {
       currentVideo: props.exampleVideoData[0],
       videoQueue: props.exampleVideoData
     };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  
+  handleClick(video) {
+    console.log('Clicked!', video);
+
+    this.setState({
+      currentVideo: video
+    });
+  }
 
   // componentDidMount() {
   //   this.setState() {
@@ -34,7 +42,7 @@ class App extends React.Component {
           <VideoPlayer video={this.state.currentVideo}/>
         </div>
         <div className="col-md-5">
-          <VideoList videos={this.state.videoQueue}/>
+          <VideoList clicked={this.handleClick} videos={this.state.videoQueue}/>
         </div>
       </div>
     </div>
@@ -47,20 +55,3 @@ class App extends React.Component {
 // `var` declarations will only exist globally where explicitly defined
 export default App;
 
-// var App = () => (
-//   <div>
-//     <nav className="navbar">
-//       <div className="col-md-6 offset-md-3">
-//         <div><h5><em>search</em> view goes here</h5></div>
-//       </div>
-//     </nav>
-//     <div className="row">
-//       <div className="col-md-7">
-//         <VideoPlayer video={exampleVideoData[0]}/>
-//       </div>
-//       <div className="col-md-5">
-//         <VideoList videos={exampleVideoData}/>
-//       </div>
-//     </div>
-//   </div>
-// );
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,12 +3,10 @@ import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map(video =>
-      <VideoListEntry video={video} />
+      <VideoListEntry video={video} clickedTitle={props.clicked}/>
     )}  
   </div>
 );
-
-
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
